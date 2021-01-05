41eaae941c73ce69d394a242e83048ca33f6a217
Sebastien Ros <sebastienros@gmail.com>
2021-01-04 19:14:57 -0800

    Fix ZeroOrMany

--- src/Parlot/Fluent/ZeroOrMany.cs
+++ src/Parlot/Fluent/ZeroOrMany.cs
@@ -63,6 +63,7 @@ namespace Parlot.Fluent
             {
                 if (first)
                 {
+                    first = false;
                     start = parsed.Start;
                 }
 
