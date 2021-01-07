e3476935ef485f6913073fb629cd0f88caf7da7f
Sebastien Ros <sebastienros@gmail.com>
2021-01-07 09:33:08 -0800

    Adding new benchmarks

---
+++ test/Parlot.Benchmarks/ParlotBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Parlot.Tests.Calc;
+
+namespace Parlot.Benchmarks
+{
+    [MemoryDiagnoser, GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory), ShortRunJob]
+    public class ParlotBenchmarks
+    {
+        private readonly Parser _parser = new();
+
+        private const string StringWithEscapes = "This is a new line \\n \\t and a tab and some \\xD83D";
+        private const string StringWithoutEscapes = "This is a new line \n \t and a tab and some \xD83D";
+
+        [Benchmark]
+        public Expression DecodeStringWithEscapes()
+        {
+            return _parser.Parse(StringWithEscapes);
+        }
+
+        [Benchmark]
+        public Expression DecodeStringWithoutEscapes()
+        {
+            return _parser.Parse(StringWithoutEscapes);
+        }
+    }
+}
