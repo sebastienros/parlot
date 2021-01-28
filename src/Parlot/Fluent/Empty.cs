1b17f53e2aab6ee1618033fd1ee90849a8a119cb
Sébastien Ros <sebastienros@gmail.com>
2021-01-27 16:08:49 -0800

    Add Empty parser (#20)

---
+++ src/Parlot/Fluent/Empty.cs
@@ -0,0 +1,29 @@
+﻿namespace Parlot.Fluent
+{
+    /// <summary>
+    /// Doesn't parse anything and return the default value.
+    /// </summary>
+    public sealed class Empty<T> : Parser<T>
+    {
+        private readonly T _value;
+
+        public Empty()
+        {
+            _value = default;
+        }
+
+        public Empty(T value)
+        {
+            _value = value;
+        }
+
+        public override bool Parse(ParseContext context, ref ParseResult<T> result)
+        {
+            context.EnterParser(this);
+
+            result.Set(context.Scanner.Cursor.Offset, context.Scanner.Cursor.Offset, _value);
+
+            return true;
+        }
+    }
+}
