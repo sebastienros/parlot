a08477aca75ec5efceaa9948bfae466ed045944f
Sébastien Ros <sebastienros@gmail.com>
2021-05-03 17:15:56 -0700

    Use Parser.SkipWhiteSpace in SkipWhiteSpace (#44)

--- test/Parlot.Tests/FluentTests.cs
+++ test/Parlot.Tests/FluentTests.cs
@@ -614,5 +614,30 @@ namespace Parlot.Tests
             Assert.True(parser.TryParse(" ab", out var result1));
             Assert.Equal(" ab", result1);
         }
+
+        [Fact]
+        public void SkipWhiteSpaceShouldResponseParseContextUseNewLines()
+        {
+            // Default behavior, newlines are skipped like any other space. The grammar is not "New Line Aware"
+
+            Assert.True(
+                SkipWhiteSpace(Literals.Text("ab"))
+                .TryParse(new ParseContext(new Scanner(" \nab"), useNewLines: false),
+                out var _, out var _));
+
+            // Here newlines are not skipped
+
+            Assert.False(
+                SkipWhiteSpace(Literals.Text("ab"))
+                .TryParse(new ParseContext(new Scanner(" \nab"), useNewLines: true),
+                out var _, out var _));
+
+            // Here newlines are not skipped, and the grammar reads them explicitly
+
+            Assert.True(
+                SkipWhiteSpace(Literals.WhiteSpace(includeNewLines: true).SkipAnd(Literals.Text("ab")))
+                .TryParse(new ParseContext(new Scanner(" \nab"), useNewLines: true),
+                out var _, out var _));
+        }
     }
 }
