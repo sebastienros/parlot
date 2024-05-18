ed9b388fa8043b4c91b14df9412a1fe9bfad4bc0
Sébastien Ros <sebastienros@gmail.com>
2024-05-18 16:19:37 -0700

    Apply PR feedback

--- src/Parlot/IsExternalInit.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace System.Runtime.CompilerServices;
-
-// c.f. https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-internal static class IsExternalInit { }
--- src/Parlot/Properties/AssemblyInfo.cs
+++ src/Parlot/Properties/AssemblyInfo.cs
@@ -1,3 +1,1 @@
-#if !NET6_0_OR_GREATER
 [module: System.Runtime.CompilerServices.SkipLocalsInit]
-#endif
